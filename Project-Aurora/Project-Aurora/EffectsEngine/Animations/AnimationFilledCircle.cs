50f8a1e730dddc1a04e92e9c312db469e5df7ee9
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-12-01 22:39:11 +0100

    fixed rainbow radial effect

--- Project-Aurora/Project-Aurora/EffectsEngine/Animations/AnimationFilledCircle.cs
+++ Project-Aurora/Project-Aurora/EffectsEngine/Animations/AnimationFilledCircle.cs
@@ -2,97 +2,96 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 
-namespace Aurora.EffectsEngine.Animations
+namespace Aurora.EffectsEngine.Animations;
+
+public class AnimationFilledCircle : AnimationCircle
 {
-    public class AnimationFilledCircle : AnimationCircle
+
+    public AnimationFilledCircle()
     {
+    }
+    public AnimationFilledCircle(AnimationCircle circleFrame) : base(circleFrame)
+    {
+    }
 
-        public AnimationFilledCircle() : base()
+    public AnimationFilledCircle(PointF center, float radius, Color color, int width = 1, float duration = 0.0f) : base(center, radius, color, width, duration)
+    {
+    }
+
+    public AnimationFilledCircle(float x, float y, float radius, Color color, int width = 1, float duration = 0.0f) : base(x, y, radius, color, width, duration)
+    {
+    }
+
+    public override void Draw(Graphics g)
+    {
+        if (_brush == null || _invalidated)
         {
-        }
-        public AnimationFilledCircle(AnimationCircle circleFrame) : base(circleFrame)
-        {
-        }
+            _brush = new SolidBrush(_color);
+            _pen = new Pen(_color);
+            _pen.Width = _width * (12 / (int)Global.Configuration.BitmapAccuracy);
+            _pen.Alignment = PenAlignment.Center;
 
-        public AnimationFilledCircle(PointF center, float radius, Color color, int width = 1, float duration = 0.0f) : base(center, radius, color, width, duration)
-        {
-        }
-
-        public AnimationFilledCircle(float x, float y, float radius, Color color, int width = 1, float duration = 0.0f) : base(x, y, radius, color, width, duration)
-        {
-        }
-
-        public override void Draw(Graphics g)
-        {
-            if (_brush == null || _invalidated)
-            {
-                _brush = new SolidBrush(_color);
-                _pen = new Pen(_color);
-                _pen.Width = _width * (12 / (int)Global.Configuration.BitmapAccuracy);
-                _pen.Alignment = PenAlignment.Center;
-
-                VirtUpdate();
-                _invalidated = false;
-            }
-
-            g.ResetTransform();
-            g.FillEllipse(_brush, _dimension);
+            VirtUpdate();
+            _invalidated = false;
         }
 
-        public override AnimationFrame BlendWith(AnimationFrame otherAnim, double amount)
+        g.ResetTransform();
+        g.FillEllipse(_brush, _dimension);
+    }
+
+    public override AnimationFrame BlendWith(AnimationFrame otherAnim, double amount)
+    {
+        if (!(otherAnim is AnimationFilledCircle))
         {
-            if (!(otherAnim is AnimationFilledCircle))
-            {
-                throw new FormatException("Cannot blend with another type");
-            }
-            AnimationFilledCircle otherCircle = (AnimationFilledCircle)otherAnim;
-
-            amount = GetTransitionValue(amount);
-
-            AnimationCircle newCircle = (AnimationCircle) base.BlendWith(otherAnim, amount);
-
-            return new AnimationFilledCircle(newCircle);
+            throw new FormatException("Cannot blend with another type");
         }
+        AnimationFilledCircle otherCircle = (AnimationFilledCircle)otherAnim;
 
-        public override AnimationFrame GetCopy()
+        amount = GetTransitionValue(amount);
+
+        AnimationCircle newCircle = (AnimationCircle) base.BlendWith(otherAnim, amount);
+
+        return new AnimationFilledCircle(newCircle);
+    }
+
+    public override AnimationFrame GetCopy()
+    {
+        return new AnimationFilledCircle(this);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((AnimationFilledCircle)obj);
+    }
+
+    public bool Equals(AnimationFilledCircle p)
+    {
+        return _color.Equals(p._color) &&
+               _dimension.Equals(p._dimension) &&
+               _width.Equals(p._width) &&
+               _duration.Equals(p._duration) &&
+               _angle.Equals(p._angle);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
         {
-            return new AnimationFilledCircle(this);
+            int hash = 17;
+            hash = hash * 23 + _color.GetHashCode();
+            hash = hash * 23 + _dimension.GetHashCode();
+            hash = hash * 23 + _width.GetHashCode();
+            hash = hash * 23 + _duration.GetHashCode();
+            hash = hash * 23 + _angle.GetHashCode();
+            return hash;
         }
+    }
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((AnimationFilledCircle)obj);
-        }
-
-        public bool Equals(AnimationFilledCircle p)
-        {
-            return _color.Equals(p._color) &&
-                _dimension.Equals(p._dimension) &&
-                _width.Equals(p._width) &&
-                _duration.Equals(p._duration) &&
-                _angle.Equals(p._angle);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 17;
-                hash = hash * 23 + _color.GetHashCode();
-                hash = hash * 23 + _dimension.GetHashCode();
-                hash = hash * 23 + _width.GetHashCode();
-                hash = hash * 23 + _duration.GetHashCode();
-                hash = hash * 23 + _angle.GetHashCode();
-                return hash;
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"AnimationFilledCircle [ Color: {_color.ToString()} Dimensions: {_dimension.ToString()} Duration: {_duration} Angle: {_angle} ]";
-        }
+    public override string ToString()
+    {
+        return $"AnimationFilledCircle [ Color: {_color.ToString()} Dimensions: {_dimension.ToString()} Duration: {_duration} Angle: {_angle} ]";
     }
 }
--- Project-Aurora/Project-Aurora/EffectsEngine/Animations/AnimationGradientCircle.cs
+++ Project-Aurora/Project-Aurora/EffectsEngine/Animations/AnimationGradientCircle.cs
@@ -2,180 +2,177 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using Newtonsoft.Json;
 
-namespace Aurora.EffectsEngine.Animations
+namespace Aurora.EffectsEngine.Animations;
+
+public class AnimationGradientCircle : AnimationCircle
 {
-    public class AnimationGradientCircle : AnimationCircle
+    public EffectBrush GradientBrush { get; }
+
+    [JsonConstructor]
+    public AnimationGradientCircle(EffectBrush gradientBrush)
     {
-        [Newtonsoft.Json.JsonProperty]
-        internal EffectBrush _gradientBrush;
+        GradientBrush = gradientBrush;
+    }
 
-        public EffectBrush GradientBrush => _gradientBrush;
-
-        public AnimationGradientCircle()
+    public AnimationGradientCircle(RectangleF dimension, EffectBrush brush, int width = 1, float duration = 0.0f) : base(dimension, Color.Transparent, width, duration)
+    {
+        GradientBrush = new EffectBrush(brush)
         {
+            start = new PointF(0.0f, 0.0f),
+            end = new PointF(1.0f, 1.0f),
+            center = new PointF(0.5f, 0.5f)
+        };
+    }
+
+    public AnimationGradientCircle(PointF center, float radius, EffectBrush brush, int width = 1, float duration = 0.0f) : base(center, radius, Color.Transparent, width, duration)
+    {
+        GradientBrush = new EffectBrush(brush)
+        {
+            start = new PointF(0.0f, 0.0f),
+            end = new PointF(1.0f, 1.0f),
+            center = new PointF(0.5f, 0.5f)
+        };
+    }
+
+    public AnimationGradientCircle(float x, float y, float radius, EffectBrush brush, int width = 1, float duration = 0.0f) : base(x, y, radius, Color.Transparent, width, duration)
+    {
+        GradientBrush = new EffectBrush(brush)
+        {
+            start = new PointF(0.0f, 0.0f),
+            end = new PointF(1.0f, 1.0f),
+            center = new PointF(0.5f, 0.5f)
+        };
+    }
+
+    protected override void VirtUpdate()
+    {
+        base.VirtUpdate();
+
+        var newColorGradients = new SortedDictionary<double, Color>();
+        var spectrum = GradientBrush.GetColorSpectrum();
+
+        if (_radius == 0)
+            return;
+        
+        var cutOffPoint = _width / _radius;
+        if (cutOffPoint < 1.0f)
+        {
+            cutOffPoint = 1.0f - cutOffPoint;
+
+            foreach (var kvp in spectrum.GetSpectrumColors())
+                newColorGradients.Add((1 - cutOffPoint) * kvp.Key + cutOffPoint, kvp.Value);
+
+            newColorGradients.Add(cutOffPoint - 0.0001f, Color.Transparent);
+            newColorGradients.Add(0.0f, Color.Transparent);
+
+            GradientBrush.colorGradients = newColorGradients;
         }
-
-        public AnimationGradientCircle(Rectangle dimension, EffectBrush brush, int width = 1, float duration = 0.0f) : base(dimension, Color.Transparent, width, duration)
+        else if (cutOffPoint > 1.0f)
         {
-            _gradientBrush = new EffectBrush(brush);
-            _gradientBrush.start = new PointF(0.0f, 0.0f);
-            _gradientBrush.end = new PointF(1.0f, 1.0f);
-            _gradientBrush.center = new PointF(0.5f, 0.5f);
-        }
-
-        public AnimationGradientCircle(RectangleF dimension, EffectBrush brush, int width = 1, float duration = 0.0f) : base(dimension, Color.Transparent, width, duration)
-        {
-            _gradientBrush = new EffectBrush(brush);
-            _gradientBrush.start = new PointF(0.0f, 0.0f);
-            _gradientBrush.end = new PointF(1.0f, 1.0f);
-            _gradientBrush.center = new PointF(0.5f, 0.5f);
-        }
-
-        public AnimationGradientCircle(PointF center, float radius, EffectBrush brush, int width = 1, float duration = 0.0f) : base(center, radius, Color.Transparent, width, duration)
-        {
-            _gradientBrush = new EffectBrush(brush);
-            _gradientBrush.start = new PointF(0.0f, 0.0f);
-            _gradientBrush.end = new PointF(1.0f, 1.0f);
-            _gradientBrush.center = new PointF(0.5f, 0.5f);
-        }
-
-        public AnimationGradientCircle(float x, float y, float radius, EffectBrush brush, int width = 1, float duration = 0.0f) : base(x, y, radius, Color.Transparent, width, duration)
-        {
-            _gradientBrush = new EffectBrush(brush);
-            _gradientBrush.start = new PointF(0.0f, 0.0f);
-            _gradientBrush.end = new PointF(1.0f, 1.0f);
-            _gradientBrush.center = new PointF(0.5f, 0.5f);
-        }
-
-        protected override void VirtUpdate()
-        {
-            base.VirtUpdate();
-
-            SortedDictionary<double, Color> newColorGradients = new SortedDictionary<double, Color>();
-            ColorSpectrum spectrum = _gradientBrush.GetColorSpectrum();
-
-            float _cutOffPoint = _width / _radius;
-            if (_cutOffPoint < 1.0f)
+            foreach (var kvp in spectrum.GetSpectrumColors())
             {
-                _cutOffPoint = 1.0f - _cutOffPoint;
-
-                foreach (var kvp in spectrum.GetSpectrumColors())
-                    newColorGradients.Add((1 - _cutOffPoint) * kvp.Key + _cutOffPoint, kvp.Value);
-
-                newColorGradients.Add(_cutOffPoint - 0.0001f, Color.Transparent);
-                newColorGradients.Add(0.0f, Color.Transparent);
-
-                _gradientBrush.colorGradients = newColorGradients;
-            }
-            else if (_cutOffPoint > 1.0f)
-            {
-                foreach (var kvp in spectrum.GetSpectrumColors())
+                if (kvp.Key >= 1 - 1 / cutOffPoint)
                 {
-                    if (kvp.Key >= (1 - 1 / _cutOffPoint))
-                    {
-                        newColorGradients.Add((1 - 1 / _cutOffPoint) * kvp.Key + _cutOffPoint, kvp.Value);
-                    }
+                    newColorGradients.Add((1 - 1 / cutOffPoint) * kvp.Key + cutOffPoint, kvp.Value);
                 }
-
-                newColorGradients.Add(0.0f, spectrum.GetColorAt((1 - 1 / _cutOffPoint)));
             }
 
-            _gradientBrush.SetBrushType(EffectBrush.BrushType.Radial);
-            _gradientBrush.center = _dimension.Location;
-            _brush = _gradientBrush.GetDrawingBrush();
-            (_brush as PathGradientBrush).TranslateTransform(
-                _dimension.X + _dimension.Width/2,
-                _dimension.Y + _dimension.Height/2
-                );
-            (_brush as PathGradientBrush).ScaleTransform(_dimension.Width, _dimension.Height);
+            newColorGradients.Add(0.0f, spectrum.GetColorAt(1 - 1 / cutOffPoint));
         }
 
-        public override void Draw(Graphics g)
+        _brush = GradientBrush.GetDrawingBrush();
+        ((PathGradientBrush)_brush).TranslateTransform(
+            _dimension.X,
+            _dimension.Y
+        );
+        ((PathGradientBrush)_brush).ScaleTransform(_dimension.Width, _dimension.Height);
+    }
+
+    public override void Draw(Graphics g)
+    {
+        if (_invalidated)
         {
-            if (_invalidated)
-            {
-                VirtUpdate();
-                _invalidated = false;
-            }
-
-            if(_brush is PathGradientBrush)
-            {
-
-                g.ResetTransform();
-                g.Transform = _transformationMatrix;
-                g.FillEllipse(_brush, _dimension);
-            }
+            VirtUpdate();
+            _invalidated = false;
         }
 
-        public override AnimationFrame BlendWith(AnimationFrame otherAnim, double amount)
+        if(_brush is PathGradientBrush)
         {
-            if (!(otherAnim is AnimationGradientCircle))
-            {
-                throw new FormatException("Cannot blend with another type");
-            }
-
-            amount = GetTransitionValue(amount);
-
-            RectangleF newrect = new RectangleF(CalculateNewValue(_dimension.X, otherAnim._dimension.X, amount),
-                CalculateNewValue(_dimension.Y, otherAnim._dimension.Y, amount),
-                CalculateNewValue(_dimension.Width, otherAnim._dimension.Width, amount),
-                CalculateNewValue(_dimension.Height, otherAnim._dimension.Height, amount)
-                );
-
-            int newwidth = CalculateNewValue(_width, otherAnim._width, amount);
-            float newAngle = CalculateNewValue(_angle, otherAnim._angle, amount);
-
-            return new AnimationGradientCircle(newrect, _gradientBrush.BlendEffectBrush((otherAnim as AnimationGradientCircle)._gradientBrush, amount), newwidth).SetAngle(newAngle);
+            g.ResetTransform();
+            g.Transform = _transformationMatrix;
+            g.FillEllipse(_brush, _dimension);
         }
+    }
 
-        public override AnimationFrame GetCopy()
+    public override AnimationFrame BlendWith(AnimationFrame otherAnim, double amount)
+    {
+        if (otherAnim is not AnimationGradientCircle circleAnim)
         {
-            RectangleF newrect = new RectangleF(_dimension.X,
-                _dimension.Y,
-                _dimension.Width,
-                _dimension.Height
-                );
-
-            return new AnimationGradientCircle(newrect, new EffectBrush(_gradientBrush), _width, _duration).SetAngle(_angle).SetTransitionType(_transitionType);
+            throw new FormatException("Cannot blend with another type");
         }
 
-        public override bool Equals(object obj)
+        amount = GetTransitionValue(amount);
+
+        var newrect = new RectangleF(CalculateNewValue(_dimension.X, circleAnim._dimension.X, amount),
+            CalculateNewValue(_dimension.Y, circleAnim._dimension.Y, amount),
+            CalculateNewValue(_dimension.Width, circleAnim._dimension.Width, amount),
+            CalculateNewValue(_dimension.Height, circleAnim._dimension.Height, amount)
+        );
+
+        var newwidth = CalculateNewValue(_width, circleAnim._width, amount);
+        var newAngle = CalculateNewValue(_angle, circleAnim._angle, amount);
+
+        return new AnimationGradientCircle(newrect, GradientBrush.BlendEffectBrush(circleAnim.GradientBrush, amount), newwidth).SetAngle(newAngle);
+    }
+
+    public override AnimationFrame GetCopy()
+    {
+        var newrect = new RectangleF(_dimension.X,
+            _dimension.Y,
+            _dimension.Width,
+            _dimension.Height
+        );
+
+        return new AnimationGradientCircle(newrect, new EffectBrush(GradientBrush), _width, _duration).SetAngle(_angle).SetTransitionType(_transitionType);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((AnimationGradientCircle)obj);
+    }
+
+    public bool Equals(AnimationGradientCircle p)
+    {
+        return _color.Equals(p._color) &&
+               _dimension.Equals(p._dimension) &&
+               _width.Equals(p._width) &&
+               _duration.Equals(p._duration) &&
+               _angle.Equals(p._angle) &&
+               GradientBrush.Equals(p.GradientBrush);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((AnimationGradientCircle)obj);
+            var hash = 17;
+            hash = hash * 23 + _color.GetHashCode();
+            hash = hash * 23 + _dimension.GetHashCode();
+            hash = hash * 23 + _width.GetHashCode();
+            hash = hash * 23 + _duration.GetHashCode();
+            hash = hash * 23 + _angle.GetHashCode();
+            hash = hash * 23 + GradientBrush.GetHashCode();
+            return hash;
         }
+    }
 
-        public bool Equals(AnimationGradientCircle p)
-        {
-            return _color.Equals(p._color) &&
-                _dimension.Equals(p._dimension) &&
-                _width.Equals(p._width) &&
-                _duration.Equals(p._duration) &&
-                _angle.Equals(p._angle) &&
-                _gradientBrush.Equals(p._gradientBrush);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 17;
-                hash = hash * 23 + _color.GetHashCode();
-                hash = hash * 23 + _dimension.GetHashCode();
-                hash = hash * 23 + _width.GetHashCode();
-                hash = hash * 23 + _duration.GetHashCode();
-                hash = hash * 23 + _angle.GetHashCode();
-                hash = hash * 23 + _gradientBrush.GetHashCode();
-                return hash;
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"AnimationGradientCircle [ Color: {_color.ToString()} Dimensions: {_dimension.ToString()} Width: {_width} Duration: {_duration} Angle: {_angle} ]";
-        }
+    public override string ToString()
+    {
+        return $"AnimationGradientCircle [ Color: {_color.ToString()} Dimensions: {_dimension.ToString()} Width: {_width} Duration: {_duration} Angle: {_angle} ]";
     }
 }
