9e11d495bf52ea0094ed8ad954b443b46501da4e
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-01-03 00:09:59 +0100

    fix chroma exclusion with new process name

--- Project-Aurora/Project-Aurora/Profiles/Chroma/ChromaApplicationSettings.cs
+++ Project-Aurora/Project-Aurora/Profiles/Chroma/ChromaApplicationSettings.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
 using Aurora.Settings;
+using Newtonsoft.Json;
 
 namespace Aurora.Profiles.Chroma;
 
+[JsonObject]
 public class ChromaApplicationSettings: ApplicationSettings
 {
-    public ObservableCollection<string> ExcludedPrograms { get; set; } = new();
-    
+    public ObservableCollection<string> ExcludedPrograms { get; } = new();
     
     [OnDeserialized]
-    void OnDeserialized(StreamingContext context)
+    private void OnDeserialized(StreamingContext context)
     {
         if (!ExcludedPrograms.Contains("Aurora.exe"))
         {
             ExcludedPrograms.Add("Aurora.exe");
+        }
+        if (!ExcludedPrograms.Contains("AuroraDeviceManager.exe"))
+        {
+            ExcludedPrograms.Add("AuroraDeviceManager.exe");
         }
     }
 }
