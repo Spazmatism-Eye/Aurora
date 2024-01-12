da34619d0497ce61c3ce6ceefd32bd89ef4079f8
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-01-12 18:03:36 +0100

    add CreateDispatcherQueueController call to transparency component

--- Project-Aurora/Project-Aurora/Utils/TransparencyComponent.cs
+++ Project-Aurora/Project-Aurora/Utils/TransparencyComponent.cs
@@ -126,6 +126,9 @@ public sealed class TransparencyComponent : IDisposable
         {
             return;
         }
+
+        WindowsSystemDispatcherQueueHelper.EnsureWindowsSystemDispatcherQueueController();
+
         SolidColorBrush brush;
         if (Global.Configuration.AllowTransparency && UseMica)
         {
@@ -147,5 +150,37 @@ public sealed class TransparencyComponent : IDisposable
         
         _lightThemeRegistryWatcher.Dispose();
         _hwHandle?.Dispose();
+    }
+}
+
+internal static class WindowsSystemDispatcherQueueHelper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    private struct DispatcherQueueOptions
+    {
+        internal int dwSize;
+        internal int threadType;
+        internal int apartmentType;
+    }
+
+    [DllImport("CoreMessaging.dll")]
+    private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object? dispatcherQueueController);
+
+    private static object? _mDispatcherQueueController;
+    public static void EnsureWindowsSystemDispatcherQueueController()
+    {
+        if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+        {
+            // one already exists, so we'll just use it.
+            return;
+        }
+
+        if (_mDispatcherQueueController != null) return;
+        DispatcherQueueOptions options;
+        options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+        options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+        options.apartmentType = 2; // DQTAT_COM_STA
+
+        CreateDispatcherQueueController(options, ref _mDispatcherQueueController);
     }
 }
