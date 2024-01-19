fbb2576339f4ccf9a08c21515741811a401e4fa3
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-01-19 04:42:53 +0100

    fix device manager for first time users

--- Project-Aurora/AuroraDeviceManager/AuroraMigration/AuroraConfiguration.cs
+++ Project-Aurora/AuroraDeviceManager/AuroraMigration/AuroraConfiguration.cs
@@ -1,20 +1,22 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using Common;
 
 namespace AuroraDeviceManager.AuroraMigration;
 
 public class AuroraConfiguration(
-    AuroraJsonList<string> enabledDevices,
-    AuroraJsonDictionary<SimpleColor> deviceCalibrations,
+    AuroraJsonList<string>? enabledDevices,
+    AuroraJsonDictionary<SimpleColor>? deviceCalibrations,
     bool allowPeripheralDevices,
     bool devicesDisableKeyboard,
     bool devicesDisableMouse,
     bool devicesDisableHeadset,
-    AuroraJsonVarRegistry varRegistry)
+    AuroraJsonVarRegistry? varRegistry)
 {
-    public AuroraJsonList<string> EnabledDevices { get; } = enabledDevices;
+    public AuroraJsonList<string> EnabledDevices { get; } = enabledDevices ?? new AuroraJsonList<string>([]);
 
-    public AuroraJsonDictionary<SimpleColor> DeviceCalibrations { get; } = deviceCalibrations;
+    public AuroraJsonDictionary<SimpleColor> DeviceCalibrations { get; } =
+        deviceCalibrations ?? new AuroraJsonDictionary<SimpleColor>(new Dictionary<string, SimpleColor>());
 
     [JsonPropertyName("allow_peripheral_devices")]
     public bool AllowPeripheralDevices { get; } = allowPeripheralDevices;
@@ -28,5 +30,5 @@ public class AuroraConfiguration(
     [JsonPropertyName("devices_disable_headset")]
     public bool DevicesDisableHeadset { get; } = devicesDisableHeadset;
     
-    public AuroraJsonVarRegistry VarRegistry { get; } = varRegistry;
+    public AuroraJsonVarRegistry VarRegistry { get; } = varRegistry ?? new AuroraJsonVarRegistry(new Dictionary<string, JsonNode>());
 }
