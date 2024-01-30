d6e52a9f65eb3beadc222525895df6e840439ad7
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-01-30 22:34:35 +0100

    add steelseries check on SteelSeries startup

--- Project-Aurora/AuroraDeviceManager/Devices/RGBNet/Implementations/SteelSeriesRgbNetDevice.cs
+++ Project-Aurora/AuroraDeviceManager/Devices/RGBNet/Implementations/SteelSeriesRgbNetDevice.cs
@@ -1,11 +1,39 @@
-﻿using RGB.NET.Core;
+﻿using AuroraDeviceManager.Utils;
+using IronPython.Runtime.Operations;
+using RGB.NET.Core;
 using RGB.NET.Devices.SteelSeries;
 
 namespace AuroraDeviceManager.Devices.RGBNet.Implementations;
 
 public class SteelSeriesRgbNetDevice() : RgbNetDevice(true)
 {
+    private static readonly string SsEngineProcess = "SteelSeriesEngine".lower();
+    private static readonly string SsGgProcess = "SteelSeriesGG".lower();
+    
+    private bool _sdkDetectedOff;
+    
     protected override IRGBDeviceProvider Provider => SteelSeriesDeviceProvider.Instance;
 
     public override string DeviceName => "SteelSeries (RGB.NET)";
+
+    protected override async Task ConfigureProvider()
+    {
+        await base.ConfigureProvider();
+        
+        var isSteelGgRunning = ProcessUtils.IsProcessRunning(SsEngineProcess);
+        var isSteelEngineRunning = ProcessUtils.IsProcessRunning(SsGgProcess);
+
+        if (!(isSteelGgRunning && isSteelEngineRunning))
+        {
+            _sdkDetectedOff = true;
+            throw new DeviceProviderException(new ApplicationException("SteelSeries Engine is not running!"), false);
+        }
+
+        if (_sdkDetectedOff)
+        {
+            await Task.Delay(5000);
+        }
+
+        _sdkDetectedOff = false;
+    }
 }
