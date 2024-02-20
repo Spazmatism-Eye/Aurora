3d330c6a8e63d3fabcb203967c1c97bad7b46236
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-02-20 20:31:37 +0100

    improve signaled memory objects

--- Project-Aurora/AuroraCommon/Data/SignaledMemoryObject.cs
+++ Project-Aurora/AuroraCommon/Data/SignaledMemoryObject.cs
@@ -1,4 +1,6 @@
-﻿using Common.Utils;
+﻿using System.Security.AccessControl;
+using System.Security.Principal;
+using Common.Utils;
 
 namespace Common.Data;
 
@@ -12,20 +14,49 @@ public class SignaledMemoryObject : IDisposable
 
     protected SignaledMemoryObject(string fileName)
     {
-        ObjectUpdatedHandle = EventWaitHandleAcl.Create(false, EventResetMode.AutoReset, fileName + "-updated", out var updated, null);
-        UpdateRequestedHandle = EventWaitHandleAcl.Create(false, EventResetMode.AutoReset, fileName + "-request", out var requested, null);
+        var updatedHandleName = fileName + "-updated";
+        var handleSecurity = GetEventWaitHandleSecurity();
 
-        if (updated)
-        {
-            ObjectUpdatedHandle.Reset();
-        }
+        const EventWaitHandleRights rights = EventWaitHandleRights.TakeOwnership | EventWaitHandleRights.Synchronize | EventWaitHandleRights.FullControl;
+        ObjectUpdatedHandle =
+            EventWaitHandleAcl.TryOpenExisting(updatedHandleName, rights, out var updatedHandle) ?
+                updatedHandle : EventWaitHandleAcl.Create(false, EventResetMode.AutoReset, updatedHandleName, out _, handleSecurity);
 
-        if (requested)
-        {
-            UpdateRequestedHandle.Reset();
-        }
+        var requestedHandleName = fileName + "-request";
+        UpdateRequestedHandle =
+            EventWaitHandleAcl.TryOpenExisting(requestedHandleName, rights, out var requestedHandle) ?
+                requestedHandle : EventWaitHandleAcl.Create(false, EventResetMode.AutoReset, requestedHandleName, out _, handleSecurity);
         
         MemorySharedEventThread.AddObject(this);
+    }
+
+    private static EventWaitHandleSecurity GetEventWaitHandleSecurity()
+    {
+        var eventWaitHandleSecurity = new EventWaitHandleSecurity();
+        
+        var anonymous = new SecurityIdentifier(WellKnownSidType.AnonymousSid, null);
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(anonymous, EventWaitHandleRights.TakeOwnership, AccessControlType.Allow)
+        );
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(anonymous, EventWaitHandleRights.Synchronize, AccessControlType.Allow)
+        );
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(anonymous, EventWaitHandleRights.FullControl, AccessControlType.Allow)
+        );
+        
+        var everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(everyone, EventWaitHandleRights.TakeOwnership, AccessControlType.Allow)
+        );
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(everyone, EventWaitHandleRights.FullControl, AccessControlType.Allow)
+        );
+        eventWaitHandleSecurity.AddAccessRule(
+            new EventWaitHandleAccessRule(everyone, EventWaitHandleRights.FullControl, AccessControlType.Allow)
+        );
+        
+        return eventWaitHandleSecurity;
     }
 
     public void RequestUpdate()
