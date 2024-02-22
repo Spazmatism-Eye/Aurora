6a25d98f19893f7f660fdc65c3372ad4bbc4fa33
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-02-22 10:16:07 +0100

    add force enable devices to Razer (RGB.NET)

--- Project-Aurora/AuroraDeviceManager/Devices/RGBNet/Implementations/RazerRgbNetDevice.cs
+++ Project-Aurora/AuroraDeviceManager/Devices/RGBNet/Implementations/RazerRgbNetDevice.cs
@@ -1,11 +1,25 @@
-﻿using RGB.NET.Core;
+﻿using Common.Devices;
 using RGB.NET.Devices.Razer;
 
 namespace AuroraDeviceManager.Devices.RGBNet.Implementations;
 
 public class RazerRgbNetDevice : RgbNetDevice
 {
-    protected override IRGBDeviceProvider Provider => RazerDeviceProvider.Instance;
+    protected override RazerDeviceProvider Provider => RazerDeviceProvider.Instance;
 
     public override string DeviceName => "Razer (RGB.NET)";
+
+    protected override async Task ConfigureProvider()
+    {
+        await base.ConfigureProvider();
+        
+        Provider.LoadEmulatorDevices = Global.DeviceConfig.VarRegistry.GetVariable<bool>($"{DeviceName}_force_all");
+    }
+
+    protected override void RegisterVariables(VariableRegistry variableRegistry)
+    {
+        base.RegisterVariables(variableRegistry);
+        
+        variableRegistry.Register($"{DeviceName}_force_all", false, "Force enable all devices");
+    }
 }
