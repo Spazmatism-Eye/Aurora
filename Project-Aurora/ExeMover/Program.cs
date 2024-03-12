8e23be046c7e627cde4b58662b12d4d76b8f50ba
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-03-12 21:46:37 +0100

    fix ExeMover

--- Project-Aurora/ExeMover/Program.cs
+++ Project-Aurora/ExeMover/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Management.Automation;
 
-// It is unfortunate but we have to set it to Unknown first.
-Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
-Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
-
-const int csidlCommonStartmenu = 0x16;  // All Users\Start Menu
+const int csidlCommonStartmenu = 0x16; // All Users\Start Menu
 const int csidlStartmenu = 0xb;
-var shell = new Shell32.Shell();
 
 var globalStartup = new StringBuilder(260);
 SHGetSpecialFolderPath(IntPtr.Zero, globalStartup, csidlCommonStartmenu, false);
@@ -26,17 +22,54 @@ if (currentPath == null)
     return;
 }
 
-string[] auroraShortcutPaths = [
+var script = """
+             param (
+                 [Parameter(Mandatory = $true)]
+                 [string]$TargetPath,
+             
+                 [Parameter(Mandatory = $true)]
+                 [string[]]$LnkFiles
+             )
+
+             foreach ($lnkFile in $LnkFiles) {
+                 # Create a Shell object
+                 $shell = New-Object -ComObject WScript.Shell
+             
+                 # Check if the file exists
+                 if (Test-Path $lnkFile) {
+                     # Get the shortcut object
+                     $shortcut = $shell.CreateShortcut($lnkFile)
+             
+                     # Change the target path
+                     $shortcut.TargetPath = $TargetPath
+             
+                     # Save the changes
+                     $shortcut.Save()
+             
+                     Write-Output "Changed target for $($lnkFile) to $TargetPath"
+                 } else {
+                     Write-Output "File $($lnkFile) does not exist."
+                 }
+             }
+
+             Write-Output "All .lnk files have been updated to target $TargetPath"
+             """;
+
+var newPath = Path.Combine(currentPath, "AuroraRgb.exe");
+string[] auroraShortcutPaths =
+[
     Path.Combine(globalStartPath, "Aurora.lnk"),
     Path.Combine(userStartPath, "Aurora.lnk"),
     Path.Combine(userStartPath, "Aurora - Shortcut.lnk"),
     Path.Combine(userStartPath, "Aurora.exe - Shortcut.lnk"),
 ];
+var lnkFiles = string.Join(", ", auroraShortcutPaths.Select(s => '"' + s + '"'));
 
-foreach (var auroraShortcutPath in auroraShortcutPaths)
-{
-    ChangeToNewPath(auroraShortcutPath);
-}
+var powershell = PowerShell.Create().AddScript(script);
+powershell.AddParameter("TargetPath", newPath);
+powershell.AddParameter("LnkFiles", lnkFiles);
+powershell.Invoke();
+
 
 Process.Start("AuroraRgb.exe");
 return;
@@ -44,27 +77,3 @@ return;
 [DllImport("shell32.dll")]
 static extern bool SHGetSpecialFolderPath(IntPtr hndOwner,
     [Out] StringBuilder lpszPath, int nFolder, bool fCreate);
-
-void ChangeToNewPath(string shortcutPath)
-{
-    var newPath = Path.Combine(currentPath, "AuroraRgb.exe");
-    if (!File.Exists(shortcutPath))
-    {
-        return;
-    }
-    ChangeLinkTarget(shortcutPath, newPath);
-}
-    
-void ChangeLinkTarget(string shortcutFullPath, string newTarget)
-{
-    // Load the shortcut.
-    var folder = shell.NameSpace(Path.GetDirectoryName(shortcutFullPath));
-    var folderItem = folder.Items().Item(Path.GetFileName(shortcutFullPath));
-    var currentLink = (Shell32.ShellLinkObject)folderItem.GetLink;
-
-    // Assign the new path here. This value is not read-only.
-    currentLink.Path = newTarget;
-
-    // Save the link to commit the changes.
-    currentLink.Save();
-}
